1c4b8276c7a0d11095e982388975bd5f17913901
Anilrocky <anilkumarac96@gmail.com>
2023-04-10 11:53:17 +0530

    [Anil] Add:Validate first name

--- UserRegistrationWithLambda/Program.cs
+++ UserRegistrationWithLambda/Program.cs
@@ -5,7 +5,24 @@ namespace UserRegistrationWithLambda
     {
         static void Main(string[] args)
         {
-
+            bool flag = true;
+            UserOperation reg = new UserOperation();
+            while (flag)
+            {
+                Console.WriteLine("\nChoose option to perform \n1.Validate first name \n2.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Enter first name");
+                        string firstName = Console.ReadLine();
+                        reg.ValidateFirstName(firstName);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
---
+++ UserRegistrationWithLambda/UserOperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationWithLambda
+{
+    public class UserOperation
+    {
+        public const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public void ValidateFirstName(string firstName)
+        {
+            if (Regex.IsMatch(firstName, NAME))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
