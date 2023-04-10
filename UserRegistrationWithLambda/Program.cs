9848754cc545756466172ab1e1f84b8b5f24cb38
Anilrocky <anilkumarac96@gmail.com>
2023-04-10 12:08:02 +0530

    [Anil] Add:Validate Mobile number

--- UserRegistrationWithLambda/Program.cs
+++ UserRegistrationWithLambda/Program.cs
@@ -9,7 +9,7 @@ namespace UserRegistrationWithLambda
             UserOperation reg = new UserOperation();
             while (flag)
             {
-                Console.WriteLine("\nChoose option to perform \n1.Validate first name \n2.Validate last name \n3.Validate E-Mail \n4.Exit");
+                Console.WriteLine("\nChoose option to perform \n1.Validate first name \n2.Validate last name \n3.Validate E-Mail \n4.Validate Phone number \n5.Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -29,6 +29,11 @@ namespace UserRegistrationWithLambda
                         reg.ValidateEmail(eMail);
                         break;
                     case 4:
+                        Console.WriteLine("Enter Phone number");
+                        string number = Console.ReadLine();
+                        reg.ValidatePhoneNumber(number);
+                        break;
+                    case 5:
                         flag = false;
                         break;
                 }
--- UserRegistrationWithLambda/UserOperation.cs
+++ UserRegistrationWithLambda/UserOperation.cs
@@ -11,6 +11,7 @@ namespace UserRegistrationWithLambda
     {
         public const string NAME = "^[A-Z]{1}[a-z]{2,}$";
         public const string EMAIL = "^[a-zA-Z]{3,}[.]{0,1}[a-zA-Z]{0,}[@]{1}[a-zA-Z]{2,}[.]{1}[a-zA-Z]{2,3}[.]{0,1}[a-zA-Z]{0,2}$";
+        public const string MOBILE_NUMBER = "^[0-9]{2}\\s[6-9]{1}[0-9]{9}$";
         public void ValidateFirstName(string firstName)
         {
             if (Regex.IsMatch(firstName, NAME))
@@ -36,6 +37,17 @@ namespace UserRegistrationWithLambda
         public void ValidateEmail(string eMail)
         {
             if (Regex.IsMatch(eMail, EMAIL))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+        public void ValidatePhoneNumber(string number)
+        {
+            if (Regex.IsMatch(number, MOBILE_NUMBER))
             {
                 Console.WriteLine("Valid");
             }
